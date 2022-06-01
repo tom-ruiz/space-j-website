29d71bd57e8c64e450cd99d300956ac4416b4f68
Tom <tomruiz2611@gmail.com>
2022-06-02 00:20:54 +0200

    route initialize

---
+++ src/components/HomeController.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeController() {
+  return (
+  <>
+      <h1>coucou</h1>
+  </>
+  );
+}
+
+export default HomeController;
---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFoundPage() {
+  return (
+  <>
+      <h1>coucou</h1>
+  </>
+  );
+}
+
+export default NotFoundPage;
